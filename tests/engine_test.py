3ad8a1925c7afca432aade21b981a3ac47e85e23
Ido Frenkel <ido123net@gmail.com>
2025-02-09 00:50:49 +0200

    Add PyFunc executable

--- tests/engine_test.py
+++ tests/engine_test.py
@@ -1,6 +1,9 @@
+import time
+
 import pytest
+
 from wtflow.engine import Engine
-from wtflow.nodes import Node
+from wtflow.nodes import Node, PyFunc
 from wtflow.workflow import Workflow
 
 pytestmark = pytest.mark.asyncio(loop_scope="module")
@@ -96,3 +99,81 @@ async def test_timeout_continue():
     engine = Engine(wf)
     assert await engine.run() == 0
     assert engine.workflow.root.outcome == "timeout"
+
+
+def func(*args, **kwargs):
+    print(args, kwargs)
+
+
+@pytest.mark.parametrize(
+    "func",
+    (func, "tests.engine_test.func"),
+)
+async def test_PyFunc_executable(func):
+    wf = Workflow(
+        name="Test Workflow",
+        root=Node(
+            name="Root Node",
+            executable=PyFunc(
+                func=func,
+                args=(1, 2),
+                kwargs={"a": 1},
+            ),
+        ),
+    )
+    engine = Engine(wf)
+    await engine.run()
+    assert engine.workflow.root.outcome == "success"
+    assert engine.workflow.root.result.retcode == 0
+    assert engine.workflow.root.result.stdout == b"(1, 2) {'a': 1}\n"
+    assert engine.workflow.root.result.stderr == b""
+
+
+def f_sleep():
+    time.sleep(2)
+    raise Exception("fail")
+
+
+async def test_PyFunc_timeout():
+    wf = Workflow(
+        name="Test Workflow",
+        root=Node(
+            name="Root Node",
+            executable=PyFunc(func=f_sleep),
+            timeout=1,
+        ),
+    )
+    engine = Engine(wf)
+    assert await engine.run() == 1
+    assert engine.workflow.root.outcome == "timeout"
+    assert engine.workflow.root.result is None
+
+
+async def test_PyFunc_exeption():
+    wf = Workflow(
+        name="Test Workflow",
+        root=Node(
+            name="Root Node",
+            executable=PyFunc(
+                func=f_sleep,
+            ),
+        ),
+    )
+    engine = Engine(wf)
+    assert await engine.run() == 1
+    assert engine.workflow.root.outcome == "failure"
+    assert engine.workflow.root.result.retcode == 1
+    assert b"fail" in engine.workflow.root.result.stderr
+
+
+async def test_cmd_or_executable():
+    with pytest.raises(ValueError) as exc:
+        Workflow(
+            name="Test Workflow",
+            root=Node(
+                name="Root Node",
+                cmd="echo 'Hello'",
+                executable=PyFunc(func=f_sleep),
+            ),
+        )
+    assert "`cmd` and `executable` are mutually exclusive" in str(exc.value)
