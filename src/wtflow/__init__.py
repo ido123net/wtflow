49ef36fb4b006983339d9fe713c0f95d178c8dcf
Ido Frenkel <ido123net@gmail.com>
2025-05-03 22:27:55 +0300

    Update README.md

--- src/wtflow/__init__.py
+++ src/wtflow/__init__.py
@@ -1,10 +1,12 @@
 from .artifact import Artifact
+from .engine import Engine
 from .executables import Command, PyFunc
 from .nodes import Node
 from .workflow import Workflow
 
 __all__ = [
     "Artifact",
+    "Engine",
     "Command",
     "PyFunc",
     "Node",
