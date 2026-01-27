0d564617a4f18c11f131eba73c4904ab6245dacc
Ido Frenkel <ido123net@gmail.com>
2026-01-27 20:20:42 +0200

    Make DB service async

--- src/wtflow/services/db/sqlite/service.py
+++ src/wtflow/services/db/sqlite/service.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-import sqlite3
-from contextlib import closing, contextmanager
+from contextlib import asynccontextmanager
 from datetime import datetime, timezone
 from pathlib import Path
-from typing import Generator
+from typing import AsyncGenerator
+
+import aiosqlite
 
 import wtflow
 from wtflow.services.db.service import DBServiceInterface
@@ -14,30 +15,29 @@ class Sqlite3DBService(DBServiceInterface):
     def __init__(self, database_path: str | Path) -> None:
         self.database_path = Path(database_path)
         self.database_path.parent.mkdir(parents=True, exist_ok=True)
-        self._create_tables()
 
-    @contextmanager
-    def _get_connection(self) -> Generator[sqlite3.Connection]:
-        sqlite3.register_adapter(datetime, self._adapt_datetime)
+    @asynccontextmanager
+    async def _get_connection(self) -> AsyncGenerator[aiosqlite.Connection]:
+        aiosqlite.register_adapter(datetime, self._adapt_datetime)
 
-        with closing(sqlite3.connect(self.database_path)) as cx:
+        async with aiosqlite.connect(self.database_path) as cx:
             yield cx
 
     @staticmethod
     def _adapt_datetime(dt: datetime) -> str:
         return dt.isoformat()
 
-    def _create_tables(self) -> None:
+    async def create_tables(self) -> None:
         schema_path = Path(__file__).parent / "schema.sql"
 
-        with self._get_connection() as conn:
+        async with self._get_connection() as conn:
             with open(schema_path, "r") as f:
                 schema_sql = f.read()
-            conn.executescript(schema_sql)
+            await conn.executescript(schema_sql)
 
-    def add_workflow(self, workflow: wtflow.Workflow) -> None:
-        def add_node(cursor: sqlite3.Cursor, node: wtflow.Node) -> None:
-            cursor.execute(
+    async def add_workflow(self, workflow: wtflow.Workflow) -> None:
+        async def add_node(cursor: aiosqlite.Cursor, node: wtflow.Node) -> None:
+            await cursor.execute(
                 "INSERT INTO nodes (name, command, lft, rgt, workflow_id) VALUES (?, ?, ?, ?, ?)",
                 (node.name, node.command, node.lft, node.rgt, workflow.id),
             )
@@ -46,12 +46,12 @@ class Sqlite3DBService(DBServiceInterface):
             node.id = row_id
 
             for child in node.children:
-                add_node(cursor, child)
+                await add_node(cursor, child)
 
-        with self._get_connection() as conn:
-            cursor = conn.cursor()
+        async with self._get_connection() as conn:
+            cursor = await conn.cursor()
 
-            cursor.execute(
+            await cursor.execute(
                 "INSERT INTO workflows (name, created_at) VALUES (?, ?)",
                 (workflow.name, datetime.now(timezone.utc)),
             )
@@ -59,29 +59,28 @@ class Sqlite3DBService(DBServiceInterface):
             assert row_id
             workflow.id = row_id
 
-            _node_dict: dict[int, tuple[int, int]] = {}
-            add_node(cursor, workflow.root)
+            await add_node(cursor, workflow.root)
 
-            conn.commit()
+            await conn.commit()
 
-    def start_execution(self, workflow: wtflow.Workflow, node: wtflow.Node) -> None:
-        with self._get_connection() as conn:
-            cursor = conn.cursor()
+    async def start_execution(self, workflow: wtflow.Workflow, node: wtflow.Node) -> None:
+        async with self._get_connection() as conn:
+            cursor = await conn.cursor()
 
-            cursor.execute(
+            await cursor.execute(
                 "INSERT INTO executions (start_at, node_id) VALUES (?, ?)",
                 (datetime.now(timezone.utc), node.id),
             )
 
-            conn.commit()
+            await conn.commit()
 
-    def end_execution(self, workflow: wtflow.Workflow, node: wtflow.Node, result: int | None = None) -> None:
-        with self._get_connection() as conn:
-            cursor = conn.cursor()
+    async def end_execution(self, workflow: wtflow.Workflow, node: wtflow.Node, result: int | None = None) -> None:
+        async with self._get_connection() as conn:
+            cursor = await conn.cursor()
 
-            cursor.execute(
+            await cursor.execute(
                 "UPDATE executions SET end_at = ?, result = ?  WHERE node_id = ?",
                 (datetime.now(timezone.utc), result, node.id),
             )
 
-            conn.commit()
+            await conn.commit()
